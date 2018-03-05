fee70140928533c776439b796bf303e58ee6c4a1
snorock <ibrovche@gmail.com>
2018-03-04 21:50:09 -0500

    finished customer model

--- app/models/customers.js
+++ app/models/customers.js
@@ -1,10 +1,53 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
-const CustomerSchema = new Schema ({
+const CustomerSchema = new Schema({
 
-    
+    username: {
+        type: String,
+        // Trim the 'username' field
+        trim: true,
+        // Set a unique 'username' index
+        unique: true,
+        // Validate 'username' value existance
+        required: true
+    },
+    firstName: String,
+    lastName: String,
+    email: {
+        type: String,
+        // Set an email index
+        index: true,
+        // Validate the email format
+        match: /.+\@.+\..+/
+    },
+    password: {
+        type: String,
+        // Validate the 'password' value length
+        validate: [
+            (password) => password.length >= 6,
+            'Password Should Be Longer'
+        ]
+    },
+    date: Date,
+    subject: String,
+    feedback: String
+});
 
+CustomerSchema.statics.findOneByUsername = function(username, callback) {
+	// Use the 'findOne' method to retrieve a user document
+	this.findOne({
+		username: new RegExp(username, 'i')
+	}, callback);
+};
+
+CustomerSchemaUserSchema.methods.authenticate = function(password) {
+	return this.password === password;
+};
+
+CustomerSchema.set('toJSON', {
+	getters: true,
+	virtuals: true
 });
 
 mongoose.model('Customer', CustomerSchema);
